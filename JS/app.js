9e5f6ea69737f280ed92be821a8b914273f0d493
Mashal <zyadomar112@gmail.com>
2025-01-05 19:25:15 +0200

    update

--- JS/app.js
+++ JS/app.js
@@ -2,3 +2,11 @@ function toggleMenu() {
     const menu = document.querySelector('.menu');
     menu.classList.toggle('open');
 }
+function openJoinUs() {
+    const openJoinUs = document.querySelector('.join_us_popup');
+    openJoinUs.style.display = "flex";
+}
+function closeJoinUs() {
+    const openJoinUs = document.querySelector('.join_us_popup');
+    openJoinUs.style.display = "none";
+}
